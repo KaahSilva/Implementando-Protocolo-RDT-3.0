c0cca6707f38ca4b5fd5fc8d97c8799e2df3fab0
Kalline Ferreira <74149487+KaahSilva@users.noreply.github.com>
2024-08-12 20:13:46 +0000

    :broom: cleanup: Limpando o código, removendo linhas inutilizadas e atualizando

--- client.py
+++ client.py
@@ -1,9 +1,53 @@
+# import socket
+# from rdt3 import RDT3_0
+
+# # Configuração do socket do cliente
+# client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# server_address = ('localhost', 8080)
+# rdt = RDT3_0(sock=client_socket)
+
+# try:
+#     # Solicita um número ao usuário
+#     message = input("Digite um número: ")
+
+#     # Envia o número para o servidor
+#     request_pkt = rdt.make_pkt(rdt.seq_num, message)
+#     rdt.udt_send(request_pkt, server_address)
+    
+#     # Espera pela resposta do servidor
+#     while True:
+#         seq_num, data, server = rdt.receive()
+#         if data is None:
+#             continue
+
+#         print(f"Resposta do servidor: {data}")
+
+#         # Envia o ACK para o servidor
+#         rdt.send_ack(seq_num, server)
+
+#         # Confere se o pacote é o esperado
+#         if seq_num == rdt.expected_seq_num:
+#             rdt.expected_seq_num = 1 - rdt.expected_seq_num  # Alterna o número de sequência esperado
+#             break
+
+# except Exception as e:
+#     print(f"Erro: {e}")
+    
+# finally:
+#     # Fecha o socket do cliente
+#     client_socket.close()
+
+
+
+
 import socket
 from rdt3 import RDT3_0
 
 # Configuração do socket do cliente
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_address = ('localhost', 8080)
+
+# Substitua 'ENDERECO_IP_SERVIDOR' pelo IP da máquina servidor
+server_address = ('ENDERECO_IP_SERVIDOR', 8080)
 rdt = RDT3_0(sock=client_socket)
 
 try:
